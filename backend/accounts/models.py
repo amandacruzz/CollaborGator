a363a63a9da40e26e612cda8551bf69e5fd7bd52
SulaymanAljaidi <sulaymanaljaidi10@gmail.com>
2024-11-18 07:12:52 +0300

    created projects table

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    languages = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, blank=True, null=True)
+    category = models.CharField(max_length=100, blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
