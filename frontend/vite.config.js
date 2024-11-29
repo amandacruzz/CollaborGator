57ca39c6f91f2c37a3d9397a15cdabdf5a89165d
Kevin Wagner <111701655+KWagnerCS@users.noreply.github.com>
2024-11-29 18:56:43 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.1.88', // Listen on all network interfaces
+    host: '0.0.0.0', // Listen on all network interfaces
     port: 5173, // The default port, but you can change it if needed
   },
 })
