6041e92337556fad9405d9a8b1693c86c3ab2027
Kevin Wagner <111701655+KWagnerCS@users.noreply.github.com>
2024-12-02 20:49:23 -0500

    prettier plugin

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from "./hooks/useAuth";
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
